7df667e945d286363f4d83ed26a51b12fb74f675
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-02 18:03:57 +0530

    commit

--- break_error.py
+++ break_error.py
@@ -1,8 +1,9 @@
 y = ["hey", 'yo', 'was', 'idk', 'sumthing']
 
 for words in y:
-    print(y)
-    if y == 'idk':
-        print("Hey IDK")
-    continue
+    print(words)
+    if words != 'idk':
+        print("The list is not equal to IDK.")
+    elif words == 'idk':
+        print("Here is where idk is!")
 
